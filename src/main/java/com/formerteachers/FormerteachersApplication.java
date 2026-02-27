4387b4af5f7e22e9abbf95a5bb5dedfaca758b58
Travis <tkarmes@gmail.com>
2026-02-27 14:28:12 -0600

    Initial Spring Boot setup for formerteachers.com

---
+++ src/main/java/com/formerteachers/FormerteachersApplication.java
@@ -0,0 +1,12 @@
+package com.formerteachers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormerteachersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FormerteachersApplication.class, args);
+    }
+}
